17e2b7a8e23bb2a939cda351b34bcb23559e20b5
John Doe <johndoe@example.com>
2020-01-19 19:23:37 +0900

    [Add]2-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	root :to => 'books#home'
+  get 'info/index'
+  get 'home/index'
+ resources :books
+ get 'home' => 'books#home'
+ get 'show' => 'books#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
